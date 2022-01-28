c88ecf09d3a551a062a35922fb8669a41481d001
Matias Nazabal <mgnazabal@gmail.com>
2022-01-27 21:23:05 -0500

    redone hw

--- hw_day_3.sql
+++ hw_day_3.sql
@@ -1,44 +1,83 @@
 -- Wednesday HW 
 -- Q1)
 
-SELECT first_name, last_name
-FROM customer
-INNER JOIN address
-ON customer.district = address.district
-WHERE customer.district LIKE 'Texas';
+SELECT customer_id, customer.customer_id, district
+FROM ADDRESS
+INNER JOIN customer
+ON customer.address_id = address.address_id
+WHERE district LIKE 'Texas';
 
 -- Q2)
 
-SELECT first_name, last_name
-FROM customer
-INNER JOIN amount
-FROM payment
+SELECT * FROM payment;
+SELECT customer.first_name, customer.last_name, 
+amount FROM payment INNER JOIN customer ON
+customer.customer_id = payment.customer_id
 WHERE amount > 6.99;
 
+-- Q3)
 
-SELECT * FROM payment;
+SELECT first_name, last_name
+FROM customer
+WHERE customer_id IN(
+	SELECT customer_id 
+	FROM payment
+	GROUP BY customer_id
+	HAVING sum(amount) > 175);
 
-SELECT customer_id
-FROM payment
-GROUP BY customer_id
-HAVING sum(amount) > 175
-ORDER BY sum(amount);
+
+-- Q4)
 
 SELECT *
 FROM customer; 
-
 SELECT first_name, last_name
 FROM customer;
+where customer_id IN (
+	SELECT customer_id 
+	FROM ADDRESS
+	INNER JOIN customer
+	ON customer.address_id  = ADDRESS.address_id 
+	INNER JOIN city
+	ON city.city_id = ADDRESS.city_id
+	INNER JOIN country
+	ON country.country_id = city.country_id 
+	WHERE country = 'Nepal'
+	);
 
-SELECT first_name, last_name, customer_id
-FROM customer 
-WHERE customer_id IN (
-	SELECT customer_id
-	FROM payment
-	GROUP BY customer_id
-	HAVING sum(amount) > 175
-	ORDER BY sum(amount);
+-- Q5)
 
-);
+SELECT first_name, last_name COUNT(rental.staff_id)
+FROM staff INNER JOIN rental
+ON staff.staff_id = rental.staff_id
+GROUP BY staff.staff_id
+ORDER BY COUNT(rental.staff_id) DESC;
 
--- Q3)
+-- Q6)
+
+SELECT rating, COUNT(rating)
+FROM film
+GROUP BY rating 
+ORDER BY COUNT(rating) DESC;
+
+-- Q7)
+
+SELECT first_name, last_name
+FROM customer
+INNER JOIN payment
+ON customer.customer_id = payment.customer_id
+WHERE payment.amount IN(
+	SELECT payment.amount
+	FROM  payment
+	WHERE payment.amount > 6.99
+	GROUP BY payment.amount)
+GROUP BY first_name, last_name
+HAVING COUNT(amount) = 1;
+SELECT COUNT(amount)
+FROM payment
+WHERE amount = 0;
+
+-- Q8)
+
+SELECT COUNT(amount)
+FROM payment
+WHERE amount = 0;
